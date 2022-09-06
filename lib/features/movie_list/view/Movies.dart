424d09379ca3620b6f1022a677bd3f065c836590
yoel <yoelsaf@gmail.com>
2022-09-06 14:28:46 +0300

    new design develop

--- lib/features/movie_list/view/Movies.dart
+++ lib/features/movie_list/view/Movies.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
+import 'package:smart_tv/features/models/movies_model.dart';
 import 'package:smart_tv/features/movie_list/utilits/text.dart';
 
 import 'package:smart_tv/features/movie_list/widgets/toprated.dart';
@@ -49,6 +52,8 @@ class _MoviesList extends State<MoviesList> {
     });
   }
 
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,15 +67,86 @@ class _MoviesList extends State<MoviesList> {
           ),
           backgroundColor: Colors.transparent,
         ),
-        body: ListView(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TrendingMovies(
-              trending: trendingmovies,
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.10,
+              //color: Colors.red,
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 90.0),
+                  child: NavigationRail(
+                      backgroundColor: Colors.transparent,
+                      indicatorColor: Colors.transparent,
+                      onDestinationSelected: (value) {
+                        setState(() {
+                          _selectedIndex = index;
+                        });
+                      },
+                      destinations: [
+                        const NavigationRailDestination(
+                            icon: Icon(
+                              Icons.home,
+                              color: Colors.amber,
+                            ),
+                            label: Text('Home')),
+                        const NavigationRailDestination(
+                            icon: Icon(Icons.search, color: Colors.amber),
+                            label: Text('search')),
+                        const NavigationRailDestination(
+                            icon: Icon(Icons.favorite, color: Colors.amber),
+                            label: Text('Favorite')),
+                        const NavigationRailDestination(
+                            icon: Icon(Icons.movie, color: Colors.amber),
+                            label: Text('Home')),
+                      ],
+                      selectedIndex: _selectedIndex)),
             ),
-            TopRated(
-              toprated: topratedmovies,
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Column(
+                  children: [
+                    Container(
+                        //  height: MediaQuery.of(context).size.height,
+                        ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Container(
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width * 0.80,
+                            decoration: BoxDecoration(
+                              // color: Colors.blue,
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: ListView(
+                              children: [
+                                TrendingMovies(
+                                  trending: trendingmovies,
+                                ),
+                                TopRated(
+                                  toprated: topratedmovies,
+                                ),
+                                TV(tv: tv),
+                                const SizedBox(
+                                  height: 100,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-            TV(tv: tv),
           ],
         ));
   }
