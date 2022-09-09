7599aa74b3f2db6684a66cbd13b47020aff0b0e9
ale-russ <agarrussom@gmail.com>
2022-09-09 16:56:58 +0300

    users are being fetched from users.json file

---
+++ lib/features/profile/models/user_models.dart
@@ -0,0 +1,58 @@
+// To parse this JSON data, do
+//
+//     final users = usersFromJson(jsonString);
+
+import 'dart:convert';
+
+Users usersFromJson(String str) => Users.fromJson(json.decode(str));
+
+String usersToJson(Users data) => json.encode(data.toJson());
+
+class Users {
+  Users({
+    this.users,
+  });
+
+  List<UserDetail>? users;
+
+  factory Users.fromJson(Map<String, dynamic> json) => Users(
+        users: List<UserDetail>.from(
+            json["users"].map((x) => UserDetail.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "users": List<dynamic>.from(users!.map((x) => x.toJson())),
+      };
+}
+
+class UserDetail {
+  UserDetail({
+    this.email,
+    this.firtName,
+    this.lastName,
+    this.sex,
+    this.country,
+  });
+
+  String? email;
+  String? firtName;
+  String? lastName;
+  String? sex;
+  String? country;
+
+  factory UserDetail.fromJson(Map<String, dynamic> json) => UserDetail(
+        email: json["email"],
+        firtName: json["firt_name"],
+        lastName: json["last_name"],
+        sex: json["sex"],
+        country: json["country"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "firt_name": firtName,
+        "last_name": lastName,
+        "sex": sex,
+        "country": country,
+      };
+}
