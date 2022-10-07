71fa95e8185426c1608a415207dd69e28cc34c02
ale-russ-kabbee <arussom@kabbee.org>
2022-10-07 20:18:22 +0300

    fixing bugs in library

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,8 @@ class MyApp extends GetView<GlobalController> {
       // },
       title: 'Kabbee Movies',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "WorkSans"),
-      home: const LoginPage(),
-      // home: MoviesPage(),
+      // home: const LoginPage(),
+      home: MoviesPage(),
       debugShowCheckedModeBanner: false,
     );
   }
